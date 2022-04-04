0bf65969c0dbd997448ec5ecc67c4224ff675fc7
Francesco Mattioli <francescomattioli@MacBook-Pro-di-Francesco.local>
2022-04-04 19:07:33 +0200

    added class AssistandCardType and class AssistantCard

--- src/main/java/it/polimi/ingsw/triton/launcher/model/AssistantCard.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/model/AssistantCard.java
@@ -1,4 +1,13 @@
 package it.polimi.ingsw.triton.launcher.model;
 
 public class AssistantCard {
+    private AssistantCardType assistantCardType;
+
+    public AssistantCard(AssistantCardType assistantCardType){
+        this.assistantCardType=assistantCardType;
+    }
+
+    public AssistantCardType getAssistantCardType() {
+        return assistantCardType;
+    }
 }
