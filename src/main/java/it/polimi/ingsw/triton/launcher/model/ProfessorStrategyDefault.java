979717cea16e360e019cddc717f37917e001d33b
Francesco Mattioli <francescomattioli@MBPdiFrancesco.fritz.box>
2022-04-04 22:18:52 +0200

    added step 7 of preparation

---
+++ src/main/java/it/polimi/ingsw/triton/launcher/model/ProfessorStrategyDefault.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.triton.launcher.model;
+
+public class ProfessorStrategyDefault implements ProfessorStrategy{
+    @Override
+    public void execute(Player player, Color color) {
+
+    }
+}
