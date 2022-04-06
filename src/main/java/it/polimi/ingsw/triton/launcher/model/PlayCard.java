9898eb67819b8d663d7514fa6467a51570e8ced3
luca-sereni <luca1.sereni@mail.polimi.it>
2022-04-06 18:04:20 +0200

    Added method removeStudentFromEntrance in schoolBoard

--- src/main/java/it/polimi/ingsw/triton/launcher/model/PlayCard.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/model/PlayCard.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.triton.launcher.model;
 
 public class PlayCard implements Action{
+    //private AssistantCard assistantCard;
+    //private AssistantDeck assistantDeck;
     @Override
     public void execute() {
         // TODO implement here
---
+++ src/test/java/it/polimi/ingsw/triton/launcher/model/MoveStudentOntoIslandTest.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.triton.launcher.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveStudentOntoIslandTest {
+
+    /**
+     * Test if the student
+     */
+    @Test
+    void execute() {
+
+    }
+}
