92a625c166c368fa3e6e617c7bf5dc28beacb6de
Luca Sereni <luca1.sereni@mail.polimi.it>
2022-04-08 17:35:24 +0200

    added useCharacterCard action (without implementation of execute()) and his test

--- src/main/java/it/polimi/ingsw/triton/launcher/model/playeractions/UseCharacterCard.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/model/playeractions/UseCharacterCard.java
@@ -1,17 +1,36 @@
 package it.polimi.ingsw.triton.launcher.model.playeractions;
 
+import it.polimi.ingsw.triton.launcher.model.Wallet;
 import it.polimi.ingsw.triton.launcher.model.cardeffects.CharacterCard;
 import it.polimi.ingsw.triton.launcher.model.playeractions.Action;
 
 public class UseCharacterCard implements Action {
     private CharacterCard characterCard;
+    private Wallet wallet;
 
-    public UseCharacterCard(CharacterCard characterCard){
+    /**
+     * @param characterCard the character card that player wants to purchase.
+     * @param wallet the amount of coins of the player
+     */
+    public UseCharacterCard(CharacterCard characterCard, Wallet wallet){
         this.characterCard = characterCard;
+        this.wallet = wallet;
+    }
+
+    /**
+     * @return true if the player has enough coins to buy the character card, false otherwise.
+     */
+    private boolean canBePurchased(){
+        return characterCard.getCost() <= wallet.getValue();
     }
 
     @Override
     public void execute() {
-        // TODO implement here
+        if(canBePurchased()){
+            characterCard.increaseCost();
+            //characterCard.executeEffect();  TODO implement here
+        }else{
+            throw new RuntimeException("You don't have enough coins");
+        }
     }
 }
---
+++ src/test/java/it/polimi/ingsw/triton/launcher/model/playeractions/UseCharacterCardTest.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.triton.launcher.model.playeractions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UseCharacterCardTest {
+
+    @Test
+    void execute() {
+    }
+}
