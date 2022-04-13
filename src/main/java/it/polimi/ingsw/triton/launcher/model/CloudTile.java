570b3b8b18f302e8e634dd47841a9f50441c5bea
engpap <andreapaparella23@gmail.com>
2022-04-13 23:53:54 +0200

    some review

--- src/main/java/it/polimi/ingsw/triton/launcher/model/CloudTile.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/model/CloudTile.java
@@ -3,20 +3,21 @@ package it.polimi.ingsw.triton.launcher.model;
 import it.polimi.ingsw.triton.launcher.model.enums.Color;
 
 public class CloudTile {
-    private int id;
-    private int[] students;
+    private final int id;
+    private final int[] students;
 
-    public CloudTile(int id){
+    public CloudTile(int id) {
         this.students = new int[5];
-        this.id=id;
+        this.id = id;
     }
 
     /**
      * set the students when the game has 3 players
+     *
      * @param x,y,z,w the students to add to the cloudTile
      */
-    public void setStudents(Color x, Color y, Color z, Color w) throws IllegalArgumentException{
-        if (x == null || y == null || z == null || w == null){
+    public void setStudents(Color x, Color y, Color z, Color w) throws IllegalArgumentException {
+        if (x == null || y == null || z == null || w == null) {
             throw new IllegalArgumentException("Color cannot be null");
         }
         students[x.ordinal()]++;
@@ -27,10 +28,11 @@ public class CloudTile {
 
     /**
      * set the students when the game has 2 players
+     *
      * @param x,y,z the students to add to the cloudTile
      */
-    public void setStudents(Color x, Color y, Color z) throws IllegalArgumentException{
-        if (x == null || y == null || z == null){
+    public void setStudents(Color x, Color y, Color z) throws IllegalArgumentException {
+        if (x == null || y == null || z == null) {
             throw new IllegalArgumentException("Color cannot be null");
         }
         students[x.ordinal()]++;
@@ -40,10 +42,11 @@ public class CloudTile {
 
     /**
      * Add a students in the cloudTile
+     *
      * @param x the student to add to the cloudTile
      */
-    public void addStudent(Color x) throws IllegalArgumentException{
-        if(x == null)
+    public void addStudent(Color x) throws IllegalArgumentException {
+        if (x == null)
             throw new IllegalArgumentException("Color cannot be null");
         else students[x.ordinal()]++;
     }
@@ -57,11 +60,11 @@ public class CloudTile {
 
     /**
      * @param student the color of the student to remove
-     * @exception if there aren't students with the specified color on the cloud tile
+     * @throws IllegalArgumentException if there aren't students with the specified color on the cloud tile
      */
-    public void removeStudentFromCloudTile(Color student){
-        if(students[student.ordinal()] > 0)
-            students[student.ordinal()] --;
+    public void removeStudentFromCloudTile(Color student) {
+        if (students[student.ordinal()] > 0)
+            students[student.ordinal()]--;
         else
             throw new IllegalArgumentException("There aren't " + student.name() + " students on this cloud tile");
     }
