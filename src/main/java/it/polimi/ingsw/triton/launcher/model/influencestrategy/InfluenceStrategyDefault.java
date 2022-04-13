6b80df2fa721f37456270fdc64fd122f693dacaa
engpap <andreapaparella23@gmail.com>
2022-04-13 17:17:30 +0200

    removed some duplicated code in influence strategy default and effect08

--- src/main/java/it/polimi/ingsw/triton/launcher/model/influencestrategy/InfluenceStrategyDefault.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/model/influencestrategy/InfluenceStrategyDefault.java
@@ -3,19 +3,9 @@ package it.polimi.ingsw.triton.launcher.model.influencestrategy;
 import it.polimi.ingsw.triton.launcher.model.Island;
 import it.polimi.ingsw.triton.launcher.model.Player;
 
-import java.util.ArrayList;
-
-public class InfluenceStrategyDefault extends InfluenceStrategy{
+public class InfluenceStrategyDefault extends InfluenceStrategyDefaultSuper {
     @Override
     public int execute(Player player, Player[] professors, Player dominator, Island island) {
-        int influence = 0;
-        if(dominator.equals(player))
-            influence+= island.getDim();
-        for(int i = 0; i<professors.length; i++){
-            if(professors[i].equals(player)){
-                influence += island.getStudents()[i];
-            }
-        }
-        return influence;
+        return super.execute(player,professors,dominator,island);
     }
 }
