86e37ace2e450ee252291d01f2fa9a304d067f3f
engpap <andreapaparella23@gmail.com>
2022-04-14 16:47:40 +0200

    added javadoc in characterCard test

--- src/test/java/it/polimi/ingsw/triton/launcher/model/cardeffects/CharacterCardTest.java
+++ src/test/java/it/polimi/ingsw/triton/launcher/model/cardeffects/CharacterCardTest.java
@@ -13,6 +13,9 @@ class CharacterCardTest {
     private Bag bag;
     private CharacterCard card;
 
+    /**
+     * This method sets up the bag for the test
+     */
     @BeforeEach
     void setUp(){
         bag = new Bag(2);
@@ -23,30 +26,46 @@ class CharacterCardTest {
         card = new CharacterCard(1,1,3,bag);
     }
 
+    /**
+     * For clarity, after each test, it sets the bag and the card at null.
+     * This is not necessary.
+     */
     @AfterEach
     void tearDown(){
         bag=null;
         card=null;
     }
 
+    /**
+     * If a null student is passed, an exception must be thrown
+     */
     @Test
     void throwsIllegalArgumentExceptionWhenDrawingNull(){
         assertThrows(IllegalArgumentException.class,()->{card.drawStudent(null);});
     }
 
+
+    /**
+     * It throws an exception if drawStudent() is called with a student color not present on the card.
+     */
     @Test
     void throwsIllegalArgumentExceptionWhenStudentNotPresent(){
         //Green is not present on the card
         assertThrows(IllegalArgumentException.class,()->{card.drawStudent(Color.GREEN);});
     }
 
+    /**
+     * Checks if the cost is increased after calling increaseCost()
+     */
     @Test
     void costIsOneUnitGraterAfterIncrease() {
         card.increaseCost();
         assertEquals(2,card.getCost());
     }
 
-
+    /**
+     * Checks if the number of students is decreased after drawing a student from the card.
+     */
     @Test
     void numberOfStudentsIsOneSmallerAfterDraw() {
         Color color=card.drawStudent(Color.BLUE);
