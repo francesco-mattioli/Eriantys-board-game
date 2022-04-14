318e1eac1a79af937b774e5472af3b18e12ab4da
Davide-Frati <davide1.frati@mail.polimi.it>
2022-04-14 15:29:17 +0200

    added some tests on mothernature

--- src/test/java/it/polimi/ingsw/triton/launcher/model/MotherNatureTest.java
+++ src/test/java/it/polimi/ingsw/triton/launcher/model/MotherNatureTest.java
@@ -1,10 +1,43 @@
 package it.polimi.ingsw.triton.launcher.model;
 
+import it.polimi.ingsw.triton.launcher.model.enums.AssistantCardType;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MotherNatureTest {
     @Test
-    void
+    void moveMotherNatureWithoutAdditionalSteps(){
+        ArrayList<Island> islands = new ArrayList<Island>();
+        for(int i=0; i<12; i++){
+            islands.add(new Island(i));
+        }
+        MotherNature motherNature = new MotherNature(islands.get(10));
+        Island island = motherNature.move(new AssistantCard(AssistantCardType.TURTLE), 10, islands);
+        assertEquals(island, islands.get(8));
+    }
+
+    @Test
+    void moveMotherNatureWithAdditionalSteps(){
+        ArrayList<Island> islands = new ArrayList<Island>();
+        for(int i=0; i<12; i++){
+            islands.add(new Island(i));
+        }
+        MotherNature motherNature = new MotherNature(islands.get(10));
+        motherNature.setAdditionalSteps(2);
+        Island island = motherNature.move(new AssistantCard(AssistantCardType.DOG), 9, islands);
+        assertEquals(island, islands.get(7));
+    }
+
+    @Test
+    void moveMotherNatureWithIllegalArgument(){
+        ArrayList<Island> islands = new ArrayList<Island>();
+        for(int i=0; i<12; i++){
+            islands.add(new Island(i));
+        }
+        MotherNature motherNature = new MotherNature(islands.get(10));
+        assertThrows(IllegalArgumentException.class, () -> {motherNature.move(new AssistantCard(AssistantCardType.EAGLE), 6, islands)});
+    }
 }
