6db4129ae0518ddcba1e0e32efd021bee9648577
Luca Sereni <luca1.sereni@mail.polimi.it>
2022-04-14 15:49:44 +0200

    modified chooseCloudTile test and javadoc

--- src/test/java/it/polimi/ingsw/triton/launcher/model/playeractions/ChooseCloudTileTest.java
+++ src/test/java/it/polimi/ingsw/triton/launcher/model/playeractions/ChooseCloudTileTest.java
@@ -5,31 +5,62 @@ import it.polimi.ingsw.triton.launcher.model.enums.Color;
 import it.polimi.ingsw.triton.launcher.model.enums.TowerColor;
 import it.polimi.ingsw.triton.launcher.model.player.SchoolBoard;
 import it.polimi.ingsw.triton.launcher.model.playeractions.ChooseCloudTile;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChooseCloudTileTest {
+    private CloudTile ct;
+    private SchoolBoard schoolBoard;
+
+    @BeforeEach
+    void setup(){
+        ct = new CloudTile(1);
+        schoolBoard = new SchoolBoard(TowerColor.BLACK);
+    }
+
+    @AfterEach
+    void tearDown(){
+        ct = null;
+        schoolBoard = null;
+    }
 
     /**
-     * Test if the students move from the chosen cloud tile to the entrance of the school board
+     * Test if the initial number of students of cloud tile is 0.
      */
     @Test
-    void execute() {
-        SchoolBoard schoolBoard = new SchoolBoard(TowerColor.BLACK);
-        CloudTile ct = new CloudTile(1);
+    void testInitialNumberStudentCloudTile() {
         ChooseCloudTile cct = new ChooseCloudTile(ct, schoolBoard);
-        assertNotNull(cct);
         assertEquals(0, ct.getStudents()[Color.BLUE.ordinal()]);
         assertEquals(0, ct.getStudents()[Color.RED.ordinal()]);
         assertEquals(0, ct.getStudents()[Color.GREEN.ordinal()]);
+    }
+
+    /**
+     * Test if the students are removed from the cloud tile.
+     */
+    @Test
+    void testIfStudentsAreRemovedFromCloudTile(){
+        ChooseCloudTile cct = new ChooseCloudTile(ct, schoolBoard);
         ct.setStudents(Color.BLUE, Color.RED, Color.GREEN);
-        assertEquals(1, ct.getStudents()[Color.BLUE.ordinal()]);
-        assertEquals(1, ct.getStudents()[Color.RED.ordinal()]);
-        assertEquals(1, ct.getStudents()[Color.GREEN.ordinal()]);
         cct.execute();
         assertEquals(0, ct.getStudents()[Color.BLUE.ordinal()]);
         assertEquals(0, ct.getStudents()[Color.RED.ordinal()]);
         assertEquals(0, ct.getStudents()[Color.GREEN.ordinal()]);
+    }
+
+    /**
+     * Test if the students are moved to the entrance of the school board.
+     */
+    @Test
+    void testIfStudentsAreMovedToEntrance(){
+        ChooseCloudTile cct = new ChooseCloudTile(ct, schoolBoard);
+        ct.setStudents(Color.BLUE, Color.RED, Color.GREEN);
+        cct.execute();
+        assertEquals(1, schoolBoard.getEntrance()[Color.BLUE.ordinal()]);
+        assertEquals(1, schoolBoard.getEntrance()[Color.RED.ordinal()]);
+        assertEquals(1, schoolBoard.getEntrance()[Color.GREEN.ordinal()]);
     }
 }
