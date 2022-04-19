860111ca7cd9073b67e41634ca209c2f96555c43
engpap <andreapaparella23@gmail.com>
2022-04-19 18:11:02 +0200

    added message

--- src/main/java/it/polimi/ingsw/triton/launcher/network/message/Message.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/network/message/Message.java
@@ -1,4 +1,12 @@
 package it.polimi.ingsw.triton.launcher.network.message;
 
-public class Message {
+public abstract class Message {
+    private final String nickname;
+    private final MessageType messageType;
+
+    public Message(String nickname, MessageType messageType) {
+        this.nickname = nickname;
+        this.messageType = messageType;
+    }
+
 }
---
+++ src/main/java/it/polimi/ingsw/triton/launcher/network/message/MessageType.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.triton.launcher.network.message;
+
+public enum MessageType {
+    LOGIN_REQUEST,
+    LOGIN_REPLY
+
+}
