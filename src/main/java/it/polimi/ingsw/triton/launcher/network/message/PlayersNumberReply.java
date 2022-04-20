630947777fdefe0b0f29712d2e5e3607f4ab547d
Luca Sereni <luca1.sereni@mail.polimi.it>
2022-04-20 14:29:58 +0200

    modified playersNumberReply with expert mode

--- src/main/java/it/polimi/ingsw/triton/launcher/network/message/PlayersNumberReply.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/network/message/PlayersNumberReply.java
@@ -2,8 +2,10 @@ package it.polimi.ingsw.triton.launcher.network.message;
 
 public class PlayersNumberReply extends Message{
     private int playersNumber;
-    public PlayersNumberReply(String nickname, int playersNumber) {
+    private boolean expertMode;
+    public PlayersNumberReply(String nickname, int playersNumber, boolean expertMode) {
         super(nickname, MessageType.PLAYERSNUMBER_REPLY);
         this.playersNumber = playersNumber;
+        this.expertMode = expertMode;
     }
 }
