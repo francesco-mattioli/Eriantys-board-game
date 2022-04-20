6c7805246af7b9d51b962562d898c43f3d75ade3
Luca Sereni <luca1.sereni@mail.polimi.it>
2022-04-20 15:29:51 +0200

    modified server class and serveOneClient class

--- src/main/java/it/polimi/ingsw/triton/launcher/network/ServeOneClient.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/network/ServeOneClient.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.triton.launcher.network;
 
 import it.polimi.ingsw.triton.launcher.controller.Controller;
+import it.polimi.ingsw.triton.launcher.network.message.Message;
+import it.polimi.ingsw.triton.launcher.network.message.MessageType;
 
 import java.awt.event.ActionEvent;
 import java.io.*;
@@ -10,16 +12,19 @@ import java.util.Observable;
 // Each player must be treated as a thread
 public class ServeOneClient implements Runnable {
     private final Socket socket;
-    private BufferedReader inSocket; // input stream for receiving messages from the client
-    private PrintWriter outSocket; //output stream for sending messages to the client
+    private final Server server;
+    private ObjectInputStream inSocket; // input stream for receiving messages from the client
+    private ObjectOutputStream outSocket; //output stream for sending messages to the client
     private Controller controller;
+    private boolean connected;
 
-    public ServeOneClient(Socket socket, Controller controller) {
-        this.socket = socket;
-        this.controller = controller;
+    public ServeOneClient(Socket socketClient, Server server) {
+        this.socket = socketClient;
+        this.server = server;
+        this.connected = true;
         try{
-            inSocket= new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            outSocket= new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+            inSocket= new ObjectInputStream(socketClient.getInputStream());
+            outSocket= new ObjectOutputStream(socketClient.getOutputStream());
         }catch (IOException e) {
             e.printStackTrace();
         }
@@ -28,29 +33,46 @@ public class ServeOneClient implements Runnable {
 
     @Override
     public void run() {
-        try{
-            try {
-                login();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } finally {
-            closeSocket();
+        try {
+            handleConnection();
+        }catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public void login() throws IOException {
+    public void handleConnection() throws IOException {
+        try{
+            while (!Thread.currentThread().isInterrupted()){
+                Message message = (Message) inSocket.readObject();
+                if(message.getMessageType() == MessageType.LOGIN_REQUEST && message != null){
+                    server.addClient(message.getNickname(), this);
+                }else{
+                    server.onReceive(message);
+                }
+            }
+        }catch(ClassCastException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        socket.close();
+    }
+
+    /*public void login() throws IOException {
         String clientSeq = inSocket.readLine();
         controller.setUsername(clientSeq);
         outSocket.println("username ok");
         controller.print();
-    }
+    }*/
 
     private void closeSocket() {
-        try {
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(connected){
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            connected = false;
+            Thread.currentThread().interrupt();
+            server.onDisconnect(this);
         }
     }
 
--- src/main/java/it/polimi/ingsw/triton/launcher/network/Server.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/network/Server.java
@@ -1,13 +1,14 @@
 package it.polimi.ingsw.triton.launcher.network;
 
 import it.polimi.ingsw.triton.launcher.controller.Controller;
+import it.polimi.ingsw.triton.launcher.network.message.Message;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static int PORT = 2000;
+    public static int PORT = 2000;   //public or private?
     private Controller controller;
 
     public Server(Controller controller) throws IOException {
@@ -19,7 +20,7 @@ public class Server {
             while (true) {
                 Socket connectionSocket = serverSocket.accept();
                 // The following thread will manage the socket that will be assigned to the Client
-                new Thread(new ServeOneClient(connectionSocket,controller)).start();
+                new Thread(new ServeOneClient(connectionSocket,this)).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -31,6 +32,22 @@ public class Server {
             }
         }
 
+    }
+
+    public void addClient(String nickname, ServeOneClient clientHandler){
+        // TODO IMPLEMENT
+    }
+
+    public void removeClient(String nickname){
+        // TODO IMPLEMENT
+    }
+
+    public void onReceive(Message message){
+        // TODO IMPLEMENT
+    }
+
+    public void onDisconnect(ServeOneClient clientHandler){
+        // TODO IMPLEMENT
     }
 
     // to run the Server
