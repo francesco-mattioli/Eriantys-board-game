a9f919ae36c514bef3c8a4c9f06e277967169521
Luca Sereni <luca1.sereni@mail.polimi.it>
2022-05-05 15:16:24 +0200

    fixed small bugs in cloudTileRequest

--- src/main/java/it/polimi/ingsw/triton/launcher/utils/message/servermessage/Requests/CloudTileRequest.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/utils/message/servermessage/Requests/CloudTileRequest.java
@@ -2,14 +2,9 @@ package it.polimi.ingsw.triton.launcher.utils.message.servermessage.Requests;
 
 
 import it.polimi.ingsw.triton.launcher.client.ServerMessageVisitor;
-import it.polimi.ingsw.triton.launcher.server.model.CloudTile;
-import it.polimi.ingsw.triton.launcher.utils.message.MessageType;
 import it.polimi.ingsw.triton.launcher.utils.message.servermessage.ServerMessage;
 
-import java.util.ArrayList;
-
 public class CloudTileRequest extends ServerMessage {
-
 
     @Override
     public void accept(ServerMessageVisitor messageVisitor) {
