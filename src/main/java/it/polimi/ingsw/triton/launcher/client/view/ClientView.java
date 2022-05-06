8f0a0ec31293828e1bcda33a0ad7eb981616cd12
Luca Sereni <luca1.sereni@mail.polimi.it>
2022-05-06 15:28:29 +0200

    fixed a bug

--- src/main/java/it/polimi/ingsw/triton/launcher/client/view/ClientView.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/client/view/ClientView.java
@@ -24,7 +24,7 @@ public interface ClientView extends View {
     void showLobbyMessage(ArrayList<String> onlineNicknames, int maxNumberPlayers );
     void showGameInfo(ArrayList<CharacterCard> availableCharacterCards, ArrayList<Island> islands, Map<String, SchoolBoard> schoolBoards, ArrayList<CloudTile> cloudTiles, Island motherNaturePosition);
 
-    void showDisconnectionMessage();
+    void showDisconnectionMessage(String disconnectedUsername);
 
     void showEmptyBagMessage();
 
