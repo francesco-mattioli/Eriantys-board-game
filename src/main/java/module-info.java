1e7a9d37f8f9a288a486017675975e37fef440ba
francesco-mattioli <francesco1.mattioli@mail.polimi.it>
2022-05-06 15:42:02 +0200

    added javafx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module it.polimi.ingsw.triton.launcher {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires java.logging;
+
+    opens it.polimi.ingsw.triton.launcher.client.gui to javafx.fxml;
+    exports it.polimi.ingsw.triton.launcher.client.gui;
+}
