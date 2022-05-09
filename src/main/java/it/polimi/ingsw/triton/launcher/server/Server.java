e9679965b53475c0426f59c5d904cd84d217645b
engpap <andreapaparella23@gmail.com>
2022-05-09 15:00:57 +0200

    EDITED TIMEOUT time

--- src/main/java/it/polimi/ingsw/triton/launcher/server/Server.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/server/Server.java
@@ -139,7 +139,7 @@ public class Server {
             while (true) {
                 Socket connectionSocket = serverSocket.accept();
                 // After 1 minute (=60'000 secs) the connection with client is closed
-                connectionSocket.setSoTimeout(10000);
+                connectionSocket.setSoTimeout(60000);
                 // The following thread will manage the socket that will be assigned to the Client
                 new Thread(new ServeOneClient(connectionSocket, this)).start();
             }
