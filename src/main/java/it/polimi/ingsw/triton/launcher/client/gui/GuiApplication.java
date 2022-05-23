26e4699f4c377cadfbc7e129c8a0aa6690fb3ed0
engpap <andreapaparella23@gmail.com>
2022-05-23 17:38:06 +0200

    introduced new variable in gui app

--- src/main/java/it/polimi/ingsw/triton/launcher/client/gui/GuiApplication.java
+++ src/main/java/it/polimi/ingsw/triton/launcher/client/gui/GuiApplication.java
@@ -1,28 +1,21 @@
 package it.polimi.ingsw.triton.launcher.client.gui;
 
-import it.polimi.ingsw.triton.launcher.client.gui.scenes.MenuSceneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-
 public class GuiApplication extends Application {
-
-    private Scene scene;
-    private Parent root;
 
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/menu-scene.fxml"));
-        root = loader.load();
-        stage = new Stage();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Parent root = loader.load();
+        Stage newStage = new Stage();
+        Scene scene = new Scene(root);
+        newStage.setScene(scene);
+        newStage.show();
     }
 
 }
